2de0d3359be4e9226e222bd49b35ac8fe3bcb7bc
doctoromer <58.omer@gmail.com>
2016-06-23 16:21:56 +0300

    Added sound
    added sound, code cleaned, and animation adjusted.

--- js/animation.js
+++ js/animation.js
@@ -1,64 +1,26 @@
-$(function() {
-    var zohar = $("#zohar")
-    var fucking = $("#fucking")
-    var hochman = $("#hochman")
+function doAnimation() {
+    $("#zohar").delay(500).show(0).fadeOut(2500);
+    $("#fucking").delay(1000).show(0).fadeOut(2000);
+    $("#hochman").delay(1500).show(0).fadeOut(1500);
 
-    zohar.
-        delay(500).
-        show(0).
-        delay(2500).
-        queue(function() {
-            zohar.remove();
-        })
+    $("#frame1").delay(3000).hide(0);
+    $("#frame2").delay(3000).show(0);
 
-    fucking.
-        delay(1000).
-        show(0).
-        delay(2000).
-        queue(function() {
-            fucking.remove();
-        })
+    $("#will-you").delay(3000).show(0);
+    $("#go-out").delay(3300).show(0);
+    $("#with-me").delay(3600).show(0);
+     $("#to-prom").delay(4200).show(0);
 
-    hochman.
-        delay(1500).
-        show(0).
-        delay(1500).
-        queue(function() {
-            hochman.remove();
-        })
+    var promSound = document.getElementById("prom-sound");
+    window.setTimeout(function() {
+        promSound.play();
+    }, 1330);
 
-    var frame1 = $("#frame1")
-    frame1.delay(3000).queue(function() {
-        frame1.remove()
-    })
-    $("#frame2").delay(3000).show(0)
+    $("#signature").delay(5400).show(0);
+    $("#in-love").delay(5400).show(0);
+    $("#doctor").delay(5800).show(0);
 
-    var go = $("#go")
-    var withMe = $("#with-me")
-    var toProm = $("#to-prom")
+    $("#frame2").delay(5800).fadeOut(400);
+}
 
-    go.
-        delay(3300).
-        show(0)
-
-    withMe.
-        delay(3600).
-        show(0)
-
-    toProm.
-        delay(4200).
-        show(0)
-
-    var signature = $("#signature")
-    var inLove = $("#in-love")
-    var doctor = $("#doctor")
-
-    signature.
-        delay(5000).
-        show(0)
-
-    doctor.
-        delay(5400).
-        show(0)
-
-})
+$(doAnimation);
